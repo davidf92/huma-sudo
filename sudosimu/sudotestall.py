6ccdf89dc47ae102624716b4403dd258905508b3
df92 <davidfnospam1@yahoo.fr>
2018-11-25 14:54:49 +0100

    Modifs env, test, testall à revoir
    Au lieu de faire une nouvelle branche pour revoir l'environnement j'ai tout mélangé dans test_main et maintenant c'est le foutoir. Besoin de tout revoir.

--- sudosimu/sudotestall.py
+++ sudosimu/sudotestall.py
@@ -1,27 +1,21 @@
+# -*- coding: utf8 -*-
+
 '''Module sudotestall du programme sudosimu : simulation de jeu de Sudoku.
-Initialise le mode de test pour l'exécution des autres modules. Cette
-initialisation permet de remplir le dictionnaire des labels de test et de
-vérifier ensuite que les niveaux correspondent aux valeurs attendues.
+Initialise le dictionnaire des labels de test pour tous les modules du
+programme. Sert à exécuter des tests avec un dictionnaire prérempli de
+ces labels pour utiliser la méthode levelAll(). Inutile en dehors de tests.
+
+Ce module n'est pas requis. Cependant pour l'utiliser il faut l'importer
+**après** l'import de sudotest.py, par exemple :
+   import sudotest
+   import sudotestall
 '''
 
-if __name__ in ("__main__", "sudotestall"):
-    import sudoenv
-elif __name__ == "sudosimu.sudotestall":
-    from sudosimu import sudoenv
+if "sudosimu." in __name__:
+    from sudosimu.sudotest import *
 else:
-    raise Exception("Impossible de faire les imports dana le module sudotestall")
-
-'''l'utilisation normale consiste à importer ce module dans un autre module
-à la suite de l'initialisation du code de test, pour utiliser l'environnement
-déjà créé et donc TEST déjà défini.
-Dans le cas contraire :
-'''
-try:
-    _dummy_ = TEST
-except:
-    env = sudoenv.SudoEnv()
-    TEST = env.test
-
+    from sudotest import *
+    
 #utilisé pour le contrôle d'exécution
 TEST.test("__nofail__")
 TEST.test("__noexcept__")
@@ -30,19 +24,18 @@ TEST.test("main")
 TEST.test("loop")
 TEST.test("step")
 TEST.test("all")
-
-#support applicatif
+#support applicatif avec l'import complet du package
 TEST.test("sudoku")
 TEST.test("app")
 TEST.test("simpleapp")
+TEST.test("simu")
+TEST.test("player")
+TEST.test("game")
 #interfaces
 TEST.test("ui")
 TEST.test("gui")
 #simulation de résolution
 TEST.test("grid")
-TEST.test("simu")
-TEST.test("player")
-TEST.test("game")
 TEST.test("observer")       #OBSOLETE
 TEST.test("gridview")
 TEST.test("memory")
@@ -65,5 +58,4 @@ TEST.test("techlplccol")
 TEST.test("techlplcsqr")
 TEST.test("techlplcplc")
 TEST.test("techlplcgrid")
-
-
+        
